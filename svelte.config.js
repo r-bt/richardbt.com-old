189d73430aa348aba8f8f3752e52465999e8a3ca
Richard Beattie <richard.beattie91@gmail.com>
2022-12-03 00:29:41 -0500

    Use netlify adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import {mdsvex} from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,11 +15,21 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter({ 
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
+		// adapter: adapter({ 
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: null,
+		// 	precompress: false
+		// }),
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+	  
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
 		}),
 		paths: { base: "" },
 		trailingSlash: 'always',
